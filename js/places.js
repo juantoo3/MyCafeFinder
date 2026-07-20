a44db394aef9198044bbc4751a9ac70d273cd405
juantoo3 <49548148+juantoo3@users.noreply.github.com>
2026-07-20 13:32:35 +0800

    Update places.js - improve fetch places

--- js/places.js
+++ js/places.js
@@ -1,29 +1,47 @@
 async function searchNearbyCafes(location) {
 
-    const radius = 3000;
+    const radius = 5000;
 
     const query = `
-    [out:json];
-    (
-      node["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
-      way["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
-      relation["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
-    );
-    out center;
-    `;
+[out:json][timeout:25];
+(
+  node["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
+  way["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
+  relation["amenity"="cafe"](around:${radius},${location.lat},${location.lng});
+);
+out center;
+`;
 
-    const url =
-        "https://overpass-api.de/api/interpreter";
+    try {
 
-    const response = await fetch(url, {
-        method: "POST",
-        body: query
-    });
+        const response = await fetch(
+            "https://overpass.kumi.systems/api/interpreter",
+            {
+                method: "POST",
+                body: query
+            }
+        );
 
-    const data = await response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+        }
 
-    displayCafeList(data.elements);
+        const data = await response.json();
 
-    addCafeMarkers(data.elements);
+        console.log(data);
+
+        displayCafeList(data.elements);
+
+        addCafeMarkers(data.elements);
+
+    }
+    catch(error){
+
+        console.error(error);
+
+        document.getElementById("cafeList").innerHTML =
+            `<p>Unable to load nearby cafes.</p>`;
+
+    }
 
 }
